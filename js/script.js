b359ed243ec5b31b05f1197ca1a503e74b955bc9
Deivison Lima <deivisontrovao@gmail.com>
2022-12-26 05:50:24 -0300

    simple-anime.js | animacao.css criados

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 const ativarLink = (link) => {
@@ -8,3 +9,62 @@ const ativarLink = (link) => {
 };
 
 links.forEach(ativarLink);
+
+// Ativar Itens do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+const ativarProduto = (parametro) => {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+};
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+/**@param {MouseEvent} event */
+
+const ativarPergunta = (event) => {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+};
+
+const eventosPerguntas = (pergunta) => {
+  pergunta.addEventListener("click", ativarPergunta);
+};
+perguntas.forEach(eventosPerguntas);
+
+//Galeria de Bicicletas
+
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+/**@param {MouseEvent} event */
+const trocarImagem = (event) => {
+  const img = event.currentTarget;
+
+  const media = window.matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+};
+
+const eventosGaleria = (img) => {
+  img.addEventListener("click", trocarImagem);
+};
+
+galeria.forEach(eventosGaleria);
+console.log(galeria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
